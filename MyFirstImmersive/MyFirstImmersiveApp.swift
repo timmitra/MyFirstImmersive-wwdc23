73be8cd4d03eb245578dfa625694d175abb74d8a
Tim Mitra <tim@it-guy.com>
2023-08-08 23:25:03 -0400

    Initial Commit

---
+++ MyFirstImmersive/MyFirstImmersiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstImmersiveApp.swift
+//  MyFirstImmersive
+//
+//  Created by Tim Mitra on 2023-08-08.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstImmersiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
